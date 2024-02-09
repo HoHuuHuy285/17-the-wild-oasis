4611790d4803c070e0cf522f4ae572d0e74c47ab
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2024-02-09 15:22:18 +0700

    Learning styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
 function App() {
   return (
     <div>
-      Hello World!!
+      <H1>The Wild Oasis</H1>
+      <Button onClick={() => alert("Check in")}>Check in</Button>
+      <Button onClick={() => alert("Check out")}>Check Out</Button>
+      <Input type="number" placeholder="Numbers of guests"></Input>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
