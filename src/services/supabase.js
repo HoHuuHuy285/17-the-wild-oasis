f3a4054e72a3f8946ad55377a79112ac73934ff5
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2024-02-11 15:15:26 +0700

    Upload Image

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://usmjqxxrlalhughopcjh.supabase.co";
+export const supabaseUrl = "https://usmjqxxrlalhughopcjh.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzbWpxeHhybGFsaHVnaG9wY2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc1Mjc4NzksImV4cCI6MjAyMzEwMzg3OX0.ts5yxbk_YXVQLq0V4i5FLW0XERDObfrAnywQbREy4oY";
 const supabase = createClient(supabaseUrl, supabaseKey);
