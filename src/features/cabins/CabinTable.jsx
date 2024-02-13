cb55db91e07833276da8f682c5242a5ca548c9dc
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2024-02-13 07:55:39 +0700

    Update edit , duplicate , delete feature

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -25,6 +26,8 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
+    <Menus>
+
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
         <div></div>
@@ -38,8 +41,9 @@ function CabinTable() {
       <Table.Body
         data={cabins}
         render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-      />
+        />
     </Table>
+        </Menus>
   );
 }
 
